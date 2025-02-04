639ad8866d3ba7223848dabb540d02e850e22684
Denys Kozachok <denys.kozachok@gmail.com>
2025-02-03 17:55:41 -0800

    Lesson 3: add arifmetics.js file

---
+++ src/lesson3/arifmetics.js
@@ -0,0 +1,24 @@
+function sum(a, b) {
+    console.log(a + b);
+
+}
+
+function sub(a, b) {
+    console.log(a - b);
+}
+
+const firstNumber = 1, secondNumber = 2;
+
+sum(firstNumber, secondNumber);
+
+const thirdNumber = 1.5, fourthNumber = 2.7;
+
+sum(thirdNumber, fourthNumber);
+
+sub(thirdNumber, fourthNumber);
+
+sum(secondNumber, fourthNumber);
+
+const fifthNumber = 1.5, sixthNumber = '2';
+
+sum(fifthNumber, sixthNumber);
