64dc94254789162d0997f05f025fb2fa49950cdb
Denys Kozachok <denys.kozachok@gmail.com>
2025-02-27 13:27:15 -0800

    Lesson 7: Fix comments from teacher

--- src/lesson7/src/arrow-functions.ts
+++ src/lesson7/src/arrow-functions.ts
@@ -4,8 +4,8 @@ const sumArrayArrow = (arr: (number | string)[]): number => {
 
 // Приклади масивів
 const numberArray2: number[] = [1, 2, 3, 4];
-const stringArray2: string[] = ["10", "20", "30"];
+const stringArray2: string[] = ['10', '20', '30'];
 
 // Виклики стрілочної функції та виведення результатів
-console.log("Sum of numberArray2 =", sumArrayArrow(numberArray2));  // 10
-console.log("Sum of stringArray2 =", sumArrayArrow(stringArray2));  // 60
+console.log('Sum of numberArray2 =', sumArrayArrow(numberArray2));  // 10
+console.log('Sum of stringArray2 =', sumArrayArrow(stringArray2));  // 60
